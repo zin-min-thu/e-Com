93ce9fb4912cc08e4dba008fa85093a3197cb574
ZinMinThu <cuzinminthu@gmail.com>
2020-08-01 20:38:06 +0630

    Add active link on sidebar

--- resources/views/layouts/admin_layout/sidebar.blade.php
+++ resources/views/layouts/admin_layout/sidebar.blade.php
@@ -25,33 +25,32 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item has-treeview menu-open">
-            <a href="{{url('admin/dashboard')}}" class="nav-link active">
+            <a href="{{url('admin/dashboard')}}" class="nav-link {{request()->is('admin/dashboard') ? 'active' : ''}}">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
-                <i class="right fas fa-angle-left"></i>
               </p>
             </a>
           </li>
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
+          <li class="nav-item has-treeview {{request()->is(['admin/settings','admin/update-detail']) ? 'menu-open' : ''}}">
+            <a href="#" class="nav-link {{request()->is(['admin/settings','admin/update-detail']) ? 'active' : ''}}">
             <i class="nav-icon fas fa-th"></i>
               <p>
-                Settings
+                Admin Settings
                 <i class="fas fa-angle-left right"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{url('admin/settings')}}" class="nav-link">
+                <a href="{{url('admin/settings')}}" class="nav-link {{request()->is('admin/settings') ? 'active' : ''}}">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Update Admin Password</p>
+                  <p>Update Password</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="{{url('admin/update-detail')}}" class="nav-link">
+                <a href="{{url('admin/update-detail')}}" class="nav-link {{request()->is('admin/update-detail') ? 'active' : ''}}">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Update Admin Detail</p>
+                  <p>Update Detail</p>
                 </a>
               </li>
             </ul>
