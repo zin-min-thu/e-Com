ac9789ef889c56dc608087e0e5416da4e70700db
ZinMinThu <cuzinminthu@gmail.com>
2020-10-25 23:32:52 +0630

    Add new favicon

--- resources/views/layouts/front_layout/layout.blade.php
+++ resources/views/layouts/front_layout/layout.blade.php
@@ -16,7 +16,7 @@
 	<!-- Google-code-prettify -->
 	<link href="{{ url ('js/front_js/gfront_css/oogle-code-prettify/prettify.css')}}" rel="stylesheet"/>
 	<!-- fav and touch icons -->
-	<link rel="shortcut icon" href="{{ url('images/front_images/ico/favicon.ico')}}">
+	<link rel="shortcut icon" href="{{ url('images/front_images/ico/favicon_e.ico')}}">
 	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ url('images/front_images/ico/apple-touch-icon-144-precomposed.png')}}">
 	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ url('images/front_images/ico/apple-touch-icon-114-precomposed.png')}}">
 	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ url('images/front_images/ico/apple-touch-icon-72-precomposed.png')}}">
