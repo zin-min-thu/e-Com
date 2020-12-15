180bceb027d27bde14e051c7b9d57c7671bd0f68
ZinMinThu <cuzinminthu@gmail.com>
2020-12-15 16:21:00 +0630

    Update admin table seeder

--- database/seeds/AdminTableSeeder.php
+++ database/seeds/AdminTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Admin;
+use Carbon\Carbon;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use Illuminate\Database\Seeder;
@@ -16,9 +17,24 @@ class AdminTableSeeder extends Seeder
      */
     public function run()
     {
-        $admin = Admin::where('type','admin')->first();
+        $admin = Admin::create([
+            'name' => 'KzShopp',
+            'type' => 'admin',
+            'mobile' => '09977899363',
+            'email' => 'kzshop.mm@gmail.com',
+            'password' => bcrypt('123456'),
+            'image' => '',
+            'status' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
 
-        $role = Role::create(['name' => 'admin']);
+        $roleCount = Role::where('name', 'admin')->count();
+        if($roleCount > 0) {
+            $role = Role::where('name', 'admin')->first();
+        } else {
+            $role = Role::create(['name' => 'admin']);
+        }
 
         $permissions = Permission::pluck('id','id')->all();
   
